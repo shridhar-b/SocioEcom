a88522f4f4b1467ce01d1016c21856d2da1d7560
Shridhar <shridhar.0305@gmail.com>
2017-05-05 13:58:36 +0530

    changed code to package as war

--- src/main/java/com/metech/socialmedia/SocialmediaApplication.java
+++ src/main/java/com/metech/socialmedia/SocialmediaApplication.java
@@ -2,13 +2,20 @@ package com.metech.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SocialmediaApplication
+public class SocialmediaApplication extends SpringBootServletInitializer
 {
 
 	public static void main(String[] args)
 	{
 		SpringApplication.run(SocialmediaApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SocialmediaApplication.class);
+	    }
 }
